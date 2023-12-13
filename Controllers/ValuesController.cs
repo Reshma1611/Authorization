c23cae45e77fc96fabc823a166bdf8be496fcc0c
Reshma <reshmabarbate@gmail.com>
2023-12-13 22:46:24 +0530

    https://github.com/Reshma1611/Authorization.git

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Authorization.Controllers
+{
+    public class ValuesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
